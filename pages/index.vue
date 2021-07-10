a50a6366e1cc78f08e40320e1de8f475cf90336c
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-07-10 14:12:48 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="main">
+  <button class="startBtn"> Start</button>
+  </main>
+</template>
+
+<script>
+export default {
+   data(){
+        return {
+            isStarted: false
+        }
+    },
+    methods: {
+        appStart(){
+            this.isStarted = true
+        }
+    }
+  
+}
+</script>
+
+<style>
+ .main{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.startBtn{
+    padding: 10px 20px;
+}
+</style>
